4438fcb7629f0d1edf17dd879c2a0a55bb3c7e86
shiyuanjia <shiyuanjiachina@163.com>
2018-07-26 15:58:11 +0800

    Initial commit

---
+++ src/main/java/org/wltea/analyzer/util/IKTokenizerFactory.java
@@ -0,0 +1,23 @@
+package org.wltea.analyzer.util;
+
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.util.TokenizerFactory;
+import org.apache.lucene.util.AttributeFactory;
+import org.wltea.analyzer.lucene.IKTokenizer;
+
+import java.util.Map;
+
+public class IKTokenizerFactory extends TokenizerFactory {
+
+    private boolean useSmart;
+
+    public IKTokenizerFactory(Map<String, String> args){
+        super(args);
+        useSmart = getBoolean(args, "useSmart", false);
+    }
+
+    public Tokenizer create(AttributeFactory attributeFactory) {
+        Tokenizer tokenizer = new IKTokenizer(useSmart);
+        return tokenizer;
+    }
+}
